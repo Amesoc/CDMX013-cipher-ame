073488a6519d4ca3a704e25a24a1cb90d4cb48f3
America hernandez <hsocamerica@gmail.com>
2022-08-02 23:00:05 -0500

    cifra sin test

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
 const cipher = {
-  // ...
-};
-
+    encode:function(textocifra,offset){
+        
+       let mensajecifrado= "";
+        for (let i=0; i< textocifra.length; i++){
+            if(textocifra.charCodeAt (i)<=90 && textocifra.charCodeAt(i)>=65){
+            let letras = (textocifra.charCodeAt(i) - 65+ (offset)) %26+65;
+            console.log(letras)
+            mensajecifrado += String.fromCharCode (letras);
+            }
+            else{
+                mensajecifrado+=textocifra[i]
+            }
+        }
+      return (mensajecifrado)
+      },
+ 
+      decode:function(textocifra2,offset){
+      let mensajedescifrado= "";
+        for (let i=0; i< textocifra2.length; i++){
+            if(textocifra2.charCodeAt (i)<=90 && textocifra2.charCodeAt(i)>=65){
+            let letras2 = textocifra2.charCodeAt(i) -(offset %26);
+            if(letras2>=40 && letras2 <=64){
+            let letras3= letras2+26
+                mensajedescifrado+=String.fromCharCode (letras3);
+            }
+            else {mensajedescifrado += String.fromCharCode (letras2)}
+            }
+            else {mensajedescifrado +=textocifra2[i]
+            }
+        }
+      return (mensajedescifrado)
+      }
+ 
+        /*nombreDelModulo:"cipher",
+        leer:function(){
+            console.log("leer")
+           
+        }*/
+       
+         
+   
+   
+ 
+ 
+ 
+ 
+ 
+    }
 export default cipher;
+ 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import cipher from './cipher.js';
-
-//definir las variables que voy a usar
-
-let mensaje1 = document.getElementById(".mensaje1");//donde va el texto a descifrar y/o dsifrar
-console.log(mensaje1)
-
-
+//traer los botones del DOM
+const botoncifrado=document.querySelector(".btncifrar");
+const botondecifrado=document.querySelector(".btndescifrar");
+ 
+console.log(botoncifrado)
+console.log(botondecifrado)
+ 
+ 
+botoncifrado.addEventListener("click",function(){
+    let texto= document.getElementById("mensajeOne").value;
+    texto = texto.toUpperCase();
+    let posicion=document.getElementById("numero").value;
+   let solucion= cipher.encode(texto,parseInt (posicion))
+   document.getElementById ("mensajeResuelto").value=solucion
+ 
+ 
+})
+ 
+botondecifrado.addEventListener("click",function(){
+    let texto=document.getElementById("mensajeOne").value;
+    texto=texto.toUpperCase();
+    let posicion=document.getElementById("numero").value;
+    let solucion= cipher.decode(texto,parseInt (posicion))
+    document.getElementById ("mensajeResuelto").value=solucion
+})
+ 
+ 
+ 
+ 
